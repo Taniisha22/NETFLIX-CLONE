5734f83fb60be986797c4b215bd6e2efd6e0a247
Reece Kenney <reecekenney50@gmail.com>
2019-10-14 22:07:25 +0100

    seasonsClass

--- includes/classes/Entity.php
+++ includes/classes/Entity.php
@@ -37,7 +37,7 @@ class Entity {
     public function getSeasons() {
         $query = $this->con->prepare("SELECT * FROM videos WHERE entityId=:id
                                     AND isMovie=0 ORDER BY season, episode ASC");
-        $query->bindValue(":id", $entity->getId());
+        $query->bindValue(":id", $this->getId());
         $query->execute();
 
         $seasons = array();
@@ -45,10 +45,21 @@ class Entity {
         $currentSeason = null;
         while($row = $query->fetch(PDO::FETCH_ASSOC)) {
             
+            if($currentSeason != null && $currentSeason != $row["season"]) {
+                $seasons[] = new Season($currentSeason, $videos);
+                $videos = array();
+            }
+
             $currentSeason = $row["season"];
             $videos[] = new Video($this->con, $row);
 
         }
+
+        if(sizeof($videos) != 0) {
+            $seasons[] = new Season($currentSeason, $videos);
+        }
+
+        return $seasons;
     }
 
 }
