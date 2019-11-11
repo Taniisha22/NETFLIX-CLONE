df0be048087c9fbe112d38346a2065103ea823ad
Reece Kenney <reecekenney50@gmail.com>
2019-11-11 20:54:49 +0000

    searchResultsProvider

---
+++ includes/classes/SearchResultsProvider.php
@@ -0,0 +1,15 @@
+<?php
+class SearchResultsProvider {
+
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function getResults($inputText) {
+        
+    }
+}
+?>
