4021402ee51e9e7948622f49401e771901b69d4a
Reece Kenney <reecekenney50@gmail.com>
2019-11-17 14:12:31 +0000

    updateAccount

--- profile.php
+++ profile.php
@@ -6,6 +6,8 @@ require_once("includes/classes/FormSanitizer.php");
 require_once("includes/classes/Constants.php");
 require_once("includes/classes/BillingDetails.php");
 
+$user = new User($con, $userLoggedIn);
+
 $detailsMessage = "";
 $passwordMessage = "";
 $subscriptionMessage = "";
@@ -55,13 +57,24 @@ if(isset($_POST["savePasswordButton"])) {
 if (isset($_GET['success']) && $_GET['success'] == 'true') {
     $token = $_GET['token'];
     $agreement = new \PayPal\Api\Agreement();
+
+    $subscriptionMessage = "<div class='alertError'>
+                            Something went wrong!
+                        </div>";
   
     try {
       // Execute agreement
       $agreement->execute($token, $apiContext);
 
         $result = BillingDetails::insertDetails($con, $agreement, $token, $userLoggedIn);
-      // Update user's account status
+        $result = $result && $user->setIsSubscribed(1);
+
+        if($result) {
+            $subscriptionMessage = "<div class='alertSuccess'>
+                            You're all signed up!
+                        </div>";
+        }
+
 
     } catch (PayPal\Exception\PayPalConnectionException $ex) {
       echo $ex->getCode();
@@ -88,7 +101,6 @@ if (isset($_GET['success']) && $_GET['success'] == 'true') {
             <h2>User details</h2>
             
             <?php
-            $user = new User($con, $userLoggedIn);
 
             $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : $user->getFirstName();
             $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : $user->getLastName();
